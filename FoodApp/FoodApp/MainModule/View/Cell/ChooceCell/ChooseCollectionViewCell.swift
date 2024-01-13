c0a608162a1cdfe03495a79152b76485310d79ef
yaprincev <5489812@mail.ru>
2024-01-13 19:31:16 +0300

    add tableview and second collection

---
+++ FoodApp/FoodApp/MainModule/View/Cell/ChooceCell/ChooseCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseCollectionViewCell.swift
+//  FoodApp
+//
+//  Created by Максим Япринцев on 13.01.2024.
+//
+
+import UIKit
+
+class ChooseCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var chooseButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- FoodApp/FoodApp/MainModule/View/MainViewController.swift
+++ FoodApp/FoodApp/MainModule/View/MainViewController.swift
@@ -8,28 +8,21 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var yOutletPromoCollection: NSLayoutConstraint!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var promoCollection: UICollectionView!
-    var images = ["Promo1", "Promo3"]
+    
+    let images = ["Promo1", "Promo3"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.titleLabel?.text = "Москва"
-        button.sizeToFit()
+        tableView.dataSource = self
+        tableView.delegate = self
         
+        configurePromoCollection()
+        configureAppearance()
         
-        
-        promoCollection.register(UINib(nibName: "\(PromoCollectionViewCell.self)", bundle: .main), forCellWithReuseIdentifier: "Cell")
-        promoCollection.dataSource = self
-        promoCollection.delegate = self
-        promoCollection.showsHorizontalScrollIndicator = false
-        
-
-        
-        if let flowLayout = promoCollection.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.scrollDirection = .horizontal
-        }
-        
-        // Do any additional setup after loading the view.
     }
 
 
@@ -38,23 +31,17 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        2
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = promoCollection.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PromoCollectionViewCell
         cell.imageCell.image = UIImage(named: images[indexPath.row])
-        print(cell.imageCell.frame.width)
-        print(cell.imageCell.frame.height)
-        cell.imageCell.layer.cornerRadius = 20
-        cell.imageCell.clipsToBounds = true
-        cell.imageCell.translatesAutoresizingMaskIntoConstraints = false
-        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 340, height: 112)
+        return CGSize(width: 350, height: 112)
         
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -66,4 +53,55 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
     }
 }
     
+/*extension MainViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == tableView {
+            let yoffset = scrollView.contentOffset.y
+            print(yOutletPromoCollection.constant)
+            yOutletPromoCollection.constant = 0
+            print(yOutletPromoCollection.constant)
+        }
+    }
+}*/
 
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "2"
+        return cell
+    }
+    
+
+    
+}
+
+extension MainViewController {
+    
+    func configureAppearance() {
+        button.sizeToFit()
+        button.titleLabel?.textColor = .black
+        button.setTitle("Москва", for: .normal)
+        button.setTitle("Москва", for: .highlighted)
+        
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.black, for: .highlighted)
+    }
+    
+    
+    
+    func configurePromoCollection() {
+        promoCollection.register(UINib(nibName: "\(PromoCollectionViewCell.self)", bundle: .main), forCellWithReuseIdentifier: "Cell")
+        promoCollection.dataSource = self
+        promoCollection.delegate = self
+        promoCollection.showsHorizontalScrollIndicator = false
+        if let flowLayout = promoCollection.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+        }
+        var initialCol = promoCollection.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).constant
+    }
+}
