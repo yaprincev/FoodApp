c2a7f54b2a67e754090b782b50446370a15c52d8
yaprincev <5489812@mail.ru>
2024-01-14 14:39:34 +0300

    did network service

---
+++ FoodApp/FoodApp/MainModule/Model/Dish.swift
@@ -0,0 +1,14 @@
+//
+//  Dish.swift
+//  FoodApp
+//
+//  Created by Максим Япринцев on 14.01.2024.
+//
+
+import Foundation
+
+struct Dish: Decodable{
+    var strMeal: String
+    var strMealThumb: String
+    var idMeal: String
+}
---
+++ FoodApp/FoodApp/MainModule/ServiceLayer/NetworkService.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkService.swift
+//  FoodApp
+//
+//  Created by Максим Япринцев on 14.01.2024.
+//
+
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func getComments(dishCategory: String, completion: @escaping (Result<[Dish]?, Error>) -> Void)
+}
+
+
+class NetrworkService: NetworkServiceProtocol {
+
+    
+    let sessionConfiguration = URLSessionConfiguration.default
+    let decoder = JSONDecoder()
+    let session = URLSession.shared
+    
+    
+    func getComments(dishCategory: String, completion: @escaping (Result<[Dish]?, Error>) -> Void) {
+        guard let url = URL(string: "www.themealdb.com/api/json/v1/1/filter.php?c=\(dishCategory)") else { return }
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            do {
+                let obj = try self.decoder.decode([Dish].self, from: data!)
+                completion(.success(obj))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
